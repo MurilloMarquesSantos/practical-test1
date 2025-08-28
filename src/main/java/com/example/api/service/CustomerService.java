651bde46e02b31a78a235e768461aaa3b39807df
Murillo Marques <murillomarques2001@gmail.com>
2025-08-28 16:30:24 -0300

    feat: adiciona verificação de campos para salvar customer

--- src/main/java/com/example/api/service/CustomerService.java
+++ src/main/java/com/example/api/service/CustomerService.java
@@ -3,9 +3,11 @@ package com.example.api.service;
 import com.example.api.domain.Customer;
 import com.example.api.domain.dto.CustomerDto;
 import com.example.api.repository.CustomerRepository;
+import com.example.api.validator.ValidateData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,9 +16,12 @@ public class CustomerService {
 
     private CustomerRepository repository;
 
+    private List<ValidateData> strategyList;
+
     @Autowired
-    public CustomerService(CustomerRepository repository) {
+    public CustomerService(CustomerRepository repository, List<ValidateData> strategies) {
         this.repository = repository;
+        this.strategyList = strategies;
     }
 
     public List<Customer> findAll() {
@@ -28,9 +33,16 @@ public class CustomerService {
     }
 
     public Customer save(CustomerDto customerDto) {
+        validateData(customerDto);
         Customer customer = new Customer();
         customer.setName(customerDto.getName());
         customer.setEmail(customerDto.getEmail());
         return repository.save(customer);
+    }
+
+    private void validateData(CustomerDto customerDto) {
+        for (ValidateData validation : strategyList) {
+            validation.execute(customerDto);
+        }
     }
 }
