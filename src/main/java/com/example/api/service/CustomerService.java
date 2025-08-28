23877f8c0d473b97f7b60c6c320da54e60945135
Murillo Marques <murillomarques2001@gmail.com>
2025-08-28 16:16:24 -0300

    feat: adiciona método para salvar customer

--- src/main/java/com/example/api/service/CustomerService.java
+++ src/main/java/com/example/api/service/CustomerService.java
@@ -1,30 +1,36 @@
 package com.example.api.service;
 
-import java.util.List;
-import java.util.Optional;
-
+import com.example.api.domain.Customer;
+import com.example.api.domain.dto.CustomerDto;
+import com.example.api.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.api.domain.Customer;
-import com.example.api.repository.CustomerRepository;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
 
-	private CustomerRepository repository;
+    private CustomerRepository repository;
 
-	@Autowired
-	public CustomerService(CustomerRepository repository) {
-		this.repository = repository;
-	}
+    @Autowired
+    public CustomerService(CustomerRepository repository) {
+        this.repository = repository;
+    }
 
-	public List<Customer> findAll() {
-		return repository.findAllByOrderByNameAsc();
-	}
+    public List<Customer> findAll() {
+        return repository.findAllByOrderByNameAsc();
+    }
 
-	public Optional<Customer> findById(Long id) {
-		return repository.findById(id);
-	}
+    public Optional<Customer> findById(Long id) {
+        return repository.findById(id);
+    }
 
+    public Customer save(CustomerDto customerDto) {
+        Customer customer = new Customer();
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        return repository.save(customer);
+    }
 }
